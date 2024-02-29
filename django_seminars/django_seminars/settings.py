d90e07f635a20d9ffdb5f24b79ea04ed32a56392
Vatsek Pavel <pvatsek@gmail.com>
2024-02-29 23:29:08 +0300

    fix

--- django_seminars/django_seminars/settings.py
+++ django_seminars/django_seminars/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+
+DEBUG = False if os.getenv('DEBUG') == 'False'else True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
